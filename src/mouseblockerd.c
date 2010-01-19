c9e52d323fc5d51f1cdd9a0b1c94a7cebdafb676
Tuomas Räsänen <tuos@codegrove.org>
2010-01-19 22:17:43 +0200

    Project structure.

--- src/mouseblockerd.c
+++ src/mouseblockerd.c
@@ -320,7 +320,7 @@ int main(int argc, char **argv)
 			break;
 		case 'V':
 			printf("mouseblockerd 0.1\n"
-			       "Copyright © 2009 Tuomas Räsänen.\n"
+			       "Copyright © 2010 Tuomas Räsänen.\n"
 			       "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
 			       "This is free software: you are free to change and redistribute it.\n"
 			       "There is NO WARRANTY, to the extent permitted by law.\n"
